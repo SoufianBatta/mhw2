30c697895e11fd972312e71b54027e4d0bda595b
Test <BTTSFN00H21E573Y@studium.unict.it>
2023-04-17 20:50:54 +0200

    Finish Commit

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 /* TODO: inserite il codice JavaScript necessario a completare il MHW! */
 const unchecked = 'images/unchecked.png';
 const checked = 'images/checked.png';
-const countValue = {};
-const valuemaps = {};
+let countValue = {};
+let valuemaps = {};
+const resetbutton = document.querySelector('button');
+
+resetbutton.addEventListener('click', Reset);
 
 const Answers = document.querySelectorAll('[data-question-id]');
 for (const answer_id of Answers){
     answer_id.addEventListener('click', Check);
 }
 
+
+function Reset(){
+    valuemaps = {};
+    countValue = {};
+    for (const answer_id of Answers){
+        answer_id.addEventListener('click', Check);
+        Uncheck(answer_id);
+        answer_id.classList.remove('transparent');
+    }
+    const footer = document.querySelector('footer');
+    footer.classList.add('hidden');
+}
 
 function AddPoint(data_choice_id){
     if(countValue[data_choice_id] == undefined){
@@ -19,12 +34,14 @@ function AddPoint(data_choice_id){
 
 function GetHigher(){
     let highest = 0;
+    let winner = null;
     for(let key in countValue){
         if(countValue[key] > highest){
             highest = countValue[key];
+            winner = key;
         }
     }
-    return highest;
+    return winner;
 }
 
 function Check(event){
@@ -64,10 +81,28 @@ function WriteResult(){
     for (const result in valuemaps){
         AddPoint(valuemaps[result]);
     }
+    disableAll();
+    fillValues();
 }
 
 function CheckAnswers(){
     if(Object.keys(valuemaps).length === 3){
         WriteResult();
     }
+}
+
+function disableAll(){
+    for (const answer of Answers){
+        answer.removeEventListener('click', Check);
+    }
+}
+
+function fillValues(){
+    const footer = document.querySelector('footer');
+    const div = footer.querySelector('div');
+    const h1 = div.querySelector('h1');
+    const span = div.querySelector('span');
+    h1.innerHTML = RESULTS_MAP[GetHigher()].title;
+    span.innerHTML = RESULTS_MAP[GetHigher()].contents;
+    footer.classList.remove('hidden');
 }
